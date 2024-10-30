b67dd69bb7d20ba7fce9981b6a94608d1011ae38
Rodrigo Fernandes <rodrigofs60@gmail.com>
2024-10-30 17:32:53 -0300

    Testes realizados na classe Program e correspondentes ao desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia(numero: "95555-5555", modelo: "N1", imei: "11111", memoria: 64);
+nokia.InstalarAplicativo("Whatsapp");
+nokia.Ligar();
+
+Smartphone iphone = new Iphone(numero: "96666-777", modelo: "Iphone 13", imei: "22222", memoria: 128);
+iphone.InstalarAplicativo("TikTok");
+iphone.ReceberLigacao();
